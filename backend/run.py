15efb7b5c025b007fd8f4ef48f6a89c5f63467d3
Ashish Mishra <i.m.ashishhh@gmail.com>
2026-09-26 09:38:01 +0530

    Fix logger initialization for Gunicorn

--- backend/run.py
+++ backend/run.py
@@ -2,7 +2,7 @@ from app import create_app
 from app.services.logger import init_logger
 
 app = create_app()
+init_logger()
 
 if __name__ == '__main__':
-    init_logger()
     app.run(debug=True, port=5002)
